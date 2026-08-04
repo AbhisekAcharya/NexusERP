d9aab82797b43ed7fcec8a64159b582194f91ef5
Abhisek Acharya <erabhisekacharya@gmail.com>
2026-08-04 12:44:43 +0530

    Started working Login Screen webpage using angular.

---
+++ frontend/nexuserp-web/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+import { Login } from './features/auth/login/login';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [Login],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  // protected readonly title = signal('nexuserp-web');
+}
